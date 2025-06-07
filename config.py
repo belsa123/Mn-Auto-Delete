dee9ceaaf1f51e5c09e0a650cdfbe1c8c3485c4b
belsa123 <herokcc25@gmail.com>
2025-06-07 20:27:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,11 +4,11 @@ load_dotenv()
 import os
 
 class BOT:
-    TOKEN = os.environ.get("TOKEN", "7589055357:AAHarvk9rGaJoHUVta4NYMOjfWf7rVQpA0E")
+    TOKEN = os.environ.get("TOKEN", "")
 
 class API:
-    HASH = os.environ.get("API_HASH", "adfd92666a3cce231cf750514a57920d")
-    ID = int(os.environ.get("API_ID", 16767636))
+    HASH = os.environ.get("API_HASH", "")
+    ID = int(os.environ.get("API_ID", ))
 
 class OWNER:
     ID = int(os.environ.get("OWNER", ))
@@ -17,5 +17,5 @@ class WEB:
     PORT = int(os.environ.get("PORT", 8000))
 
 class CHATS:
-    IDS = list(map(int, os.environ.get("CHATS", "-1002242134235 -1002284460452").split())) if os.environ.get("CHATS") else []
+    IDS = list(map(int, os.environ.get("CHATS", "").split())) if os.environ.get("CHATS") else []
     DELETE_DELAY = int(os.environ.get("DELETE_DELAY", "60")) 
